bb51f33ef534bc537adefc5a411bd4fe8c29456a
morgancollado <Morgan.collado@gmail.com>
2020-11-26 15:02:31 -0600

    commit tree aa18b531098efeeab240e92873c975da8b4a7c7f

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special", ) {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: function(a,b) {return a + b},
+    subtract: function(a,b) {return a - b},
+    multiply: function(a,b) {return a * b},
+    divide: function(a,b) {return a/b}
+}
+
+function actionApplyer(start, arr){
+    return arr.reduce(function(res,fn){
+        return fn(res)
+    },start)
+}
